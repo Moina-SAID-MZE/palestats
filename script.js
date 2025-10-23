5afa622ca15d72ca2f7f22b45ac6cfcaf95dc282
Mna <moina.tsm@gmail.com>
2025-10-23 15:09:43 +0200

    Update script.js

--- script.js
+++ script.js
@@ -13,7 +13,7 @@ document.addEventListener('DOMContentLoaded', () => {
 
   if (!section || !zone) return;
 
-  fetch('noms.csv')
+  fetch('données/noms.csv')
     .then(r => r.text())
     .then(txt => {
       const lignes = txt.split('\n').map(l => l.trim()).filter(Boolean);
